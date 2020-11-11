e09f4eadacd08fd4741bea2d1b2a812128105b4c
Rebeca Lima Rocha <rebeca.rocha@liven.tech>
2020-11-10 22:05:35 -0300

    feat: create user resource

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, :email, presence: true
+  validates :password, length: { minimum: 4 }
+  validates :age, numericality: { only_integer: true }
+
+  def as_json(options={})
+    super(options.merge({ except: [:password_digest] }))
+  end
+end
